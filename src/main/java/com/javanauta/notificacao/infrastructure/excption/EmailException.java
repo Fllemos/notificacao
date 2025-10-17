5afeee055ff2a64d8a300cfdd73e0411e18e93cd
fernando lemo <fernandlemos@hotmail.com>
2025-10-17 14:32:15 -0300

    add metodos de envio de email

---
+++ src/main/java/com/javanauta/notificacao/infrastructure/excption/EmailException.java
@@ -0,0 +1,14 @@
+package com.javanauta.notificacao.infrastructure.excption;
+
+public class EmailException extends RuntimeException {
+
+    public EmailException(String mensagem) {
+        super(mensagem);
+    }
+
+    public EmailException(String mensagem, Throwable throwable) {
+        super(mensagem, throwable);
+
+
+    }
+}
